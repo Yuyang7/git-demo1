4171b466281f6f5015583a52cfb6adf3446f8a9b
yuyang <yuyang@yuyang.com>
2022-09-06 16:06:15 +0800

    first commit

---
+++ src/main/java/com/yuyang/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yuyang.git;
+
+/**
+ * @author yu yang
+ * @date 2022/9/6 -15:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git!");
+    }
+}
